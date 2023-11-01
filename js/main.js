0ed06f2d2bbdc82e633f1d2be082dde71e24ebcf
DaniCcimo <bessmertnyj228@gmail.com>
2023-11-01 15:31:39 +0200

    Finished header and footer

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// Имя страници в логотипе
+let titleName = document.title;
+let titleText = document.querySelector(`.web-title`)
+console.log(titleName);
+console.log(titleText);
+
+if (document.title =! `Home`) {
+    document.title = titleName;
+} else {
+    document.title = `Home`;
+    titleText.style.display = `none`;
+}
+
+// Активная ссылка в header nav link
+let b = window.location.href;
+let count = document.querySelector(`.header__nav--list`).querySelectorAll(`.header__nav--item a`).length;
+
+for (let i = 0; i < count; i++) {
+    let c = document.querySelectorAll(`.header__nav--link`)[i];
+    let d = c.href;
+    if (d == b) {
+      c.classList.add(`header__nav--link_target`);
+    } else {
+      c.classList.remove(`header__nav--link_target`);
+    }
+}
+
+// Отмена перехода к табу при нажатии
+let tabs = document.querySelectorAll(`.hero__experience--link`);
+let tabsContent = document.querySelectorAll(`.hero__experience--item--content`);
+
+tabs.forEach(tab => {
+    tab.addEventListener(`click`, function (e) {
+        e.preventDefault();
+        const id = e.target.getAttribute(`href`).replace(`#`, ``);
+
+        tabs.forEach(tabItem => {
+            tabItem.classList.remove(`hero__experience--link--target`);
+        });
+        tabsContent.forEach(tabItem => {
+            tabItem.classList.remove(`hero__experience--item--content--target`);
+        });
+        tab.classList.add(`hero__experience--link--target`);
+        document.getElementById(id).classList.add(`hero__experience--item--content--target`);
+    })
+});
+
+document.querySelector(`.hero__experience--link`).click();
