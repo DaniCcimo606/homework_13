8aaad0dfbaa0452de02eaba31509756572ceafed
DaniCcimo <bessmertnyj228@gmail.com>
2023-11-05 13:27:25 +0200

    Finished Publications page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 // Имя страници в логотипе
 let titleName = document.title;
-let titleText = document.querySelector(`.web-title`)
+let webTitle = document.querySelector(`.web-title`)
+let titleText = document.querySelector(`.title__name`)
 
-if (document.title =! `Home`) {
+if (document.title != `Home`) {
     document.title = titleName;
+    titleText.textContent = titleName;
 } else {
     document.title = `Home`;
-    titleText.style.display = `none`;
+    webTitle.style.display = `none`;
 }
 
 // Активная ссылка в header nav link
@@ -27,20 +29,49 @@ for (let i = 0; i < count; i++) {
 let tabs = document.querySelectorAll(`.hero__experience--link`);
 let tabsContent = document.querySelectorAll(`.hero__experience--item--content`);
 
-tabs.forEach(tab => {
-    tab.addEventListener(`click`, function (e) {
-        e.preventDefault();
-        const id = e.target.getAttribute(`href`).replace(`#`, ``);
+if (document.title == `Home`) {
+    tabs.forEach(tab => {
+        tab.addEventListener(`click`, function (e) {
+            e.preventDefault();
+            const id = e.target.getAttribute(`href`).replace(`#`, ``);
+    
+            tabs.forEach(tabItem => {
+                tabItem.classList.remove(`hero__experience--link--target`);
+            });
+            tabsContent.forEach(tabItem => {
+                tabItem.classList.remove(`hero__experience--item--content--target`);
+            });
+            tab.classList.add(`hero__experience--link--target`);
+            document.getElementById(id).classList.add(`hero__experience--item--content--target`);
+        })
+    });
+    document.querySelector(`.hero__experience--link`).click();
+}
 
-        tabs.forEach(tabItem => {
-            tabItem.classList.remove(`hero__experience--link--target`);
-        });
-        tabsContent.forEach(tabItem => {
-            tabItem.classList.remove(`hero__experience--item--content--target`);
-        });
-        tab.classList.add(`hero__experience--link--target`);
-        document.getElementById(id).classList.add(`hero__experience--item--content--target`);
-    })
-});
 
-document.querySelector(`.hero__experience--link`).click();
+// Добавляю изменяю макс. ширину для страниц
+let c = document.querySelector(`.header`);
+let container = document.querySelectorAll(`.container`);
+
+if (document.title == `Team`, `Publications`) {
+    c.style.position = `inherit`;
+    container.forEach(element => {
+        element.style.maxWidth = `1600px`;
+        element.style.margin = `0 auto`;
+    });
+}
+
+// Кнопка "news" "articles" на странице Publications
+let publickFloatBg = document.querySelector(`.floating-bg`);
+let news = document.getElementById(`news`);
+let articles = document.getElementById(`articles`);
+
+news.addEventListener(`click`, function () {
+    publickFloatBg.style.width = `50%`;
+})
+articles.addEventListener(`click`, function () {
+    publickFloatBg.style.width = `100%`;
+})
+if (document.title == `Publications`) {
+    news.click();
+}
